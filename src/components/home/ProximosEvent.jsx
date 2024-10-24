c3db03733ab937961b2758a788a0d67d751743c8
Luiz Henrique <luizhenriqueee31@gmail.com>
2024-10-24 18:38:24 -0300

    Calendário

--- src/components/home/ProximosEvent.jsx
+++ src/components/home/ProximosEvent.jsx
@@ -1,10 +1,12 @@
 import styles from "@/styles/Home.module.css";
+import Calendario from "../calendario/Calendario";
 
 export default function ProximosEvent() {
   return (
     <div className={styles.proximosEventos}>
       <h2>Próximos Eventos</h2>
       <p>Aqui estar  os eventos recentes</p>
+      <Calendario tamanho="pequeno"/>
     </div>
   );
 }
